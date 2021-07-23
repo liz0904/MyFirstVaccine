e616daaf1c769129edc0ee3fac3f0e2714ba3211
SungYeonKim <60651715+liz0904@users.noreply.github.com>
2021-07-23 17:09:15 +0900

    파일 크기를 활용해 검사속도 향상 시키기

--- antivirus.py
+++ antivirus.py
@@ -1,68 +1,62 @@
-<<<<<<< HEAD
-import hashlib
-import os
-
-fp=open('eicar.txt', 'rb')  #read as binary
-fbuf=fp.read()
-fp.close()
-
-m=hashlib.md5() 
-m.update(fbuf)  # get MD5 hash of fbuf
-fmd5=m.hexdigest()
-
-if fmd5 == '44d88612fea8a8f36de82e1278abb02f':  #Compare with MD5 of eicar.txt
-    print 'Virus'
-    os.remove('eicar.txt')  #remove antiVirus File
-else :
-    print 'No Virus'
-=======
 #-*-coding:utf-8 -*-
 import sys  
 import hashlib
 import os
 
 VirusDB=[   #MD5 Hash: Name of Virus
-    '44d88612fea8a8f36de82e1278abb02f:EICAR Test', #eicar.txt
-    '99e22455ab3caa37e63a262eceb443dd: Dummy Test'  #dummy.txt
+    '68:44d88612fea8a8f36de82e1278abb02f:EICAR Test', #eicar.txt
+    '62:99e22455ab3caa37e63a262eceb443dd: Dummy Test'  #dummy.txt
 ]
 
-vdb=[]
+vdb=[]  #가공된 악성코드 DB 저장
+vsize=[]    #악성코드의 파일 크기 저장
 
 def MakeVirusDB():
     for pattern in VirusDB:
         t=[]
         v = pattern.split(':')   
-        t.append(v[0]) #Add MD5 Hash
-        t.append(v[1]) #Add Name of Virus
+        t.append(v[1]) #Add MD5 Hash
+        t.append(v[2]) #Add Name of Virus
         vdb.append(t)   #Save to vdb
-       
+        
+        size=int(v[0]) #악성코드 파일 크기
+        if vsize.count(size)==0:
+            vsize.append(size)
+ 
+#악성코드 검사 함수 
 def SearchVDB(fmd5):
     for t in vdb:
         if t[0]==fmd5:  #Compare MD5
             return True, t[1]   #Return Virus name
             
     return False, ''    #No Virus
-    
+
 if __name__=='__main__':
     MakeVirusDB()
     
+    #커맨드라인 입력 방식 체크
     if len(sys.argv) !=2:
         print 'Usage: antivirus.py [file]'
         exit(0)
        
-    fname = sys.argv[1]
-    fp=open(fname, 'rb')  #read as binary
-    buf=fp.read()
-    fp.close()
+    fname = sys.argv[1] #악성코드 검사 대상 파일
+    
+    size=os.path.getsize(fname) #검사 대상 파일 크기 구하기
+    if vsize.count(size):
+        fp=open(fname, 'rb')  #read as binary
+        buf=fp.read()
+        fp.close()
 
-    m=hashlib.md5() 
-    m.update(buf)  # get MD5 hash of buf
-    fmd5=m.hexdigest()
+        m=hashlib.md5() 
+        m.update(buf)  # get MD5 hash of buf
+        fmd5=m.hexdigest()
 
-    ret, vname=SearchVDB(fmd5)  # find Virus
-    if ret == True:  #Compare with MD5
-        print '%s: %s' % (fname, vname)
-        os.remove(fname)  #remove antiVirus File
+        ret, vname=SearchVDB(fmd5)  # find Virus
+        if ret == True:  #Compare with MD5
+            print '%s: %s' % (fname, vname)
+            os.remove(fname)  #remove antiVirus File
+        else :
+            print '%s : ok' %(fname)
     else :
-        print '%s : ok' %(fname)
->>>>>>> 4b2f061 (악성코드 DB 리스트 추가)
+            print '%s : ok' %(fname)
+
