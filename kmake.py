a0800b6e48b2df8ae2c306f3ba4a6d2ef233be70
liz0904 <liz09045@naver.com>
2021-08-03 23:17:46 +0900

    악성코드 패턴 파일 암호화 도구

--- kmake.py
+++ kmake.py
@@ -1,12 +1,3 @@
-'''
-<virus.db 파일은 누구나 편집이 가능하기 때문에 해커가 마음대로 조작하지 못하게 암호화해주는 과정>
-1. 암호 대상 파일을 인자값으로 입력받은 뒤, 만약 인자값이 없다면 오류메세지 출력
-2. 정상적으로 입력되었다면, 파일 전체 내용을 읽은 다음 zlib으로 압축
-3. 압축된 내용을 1 byte 단위로 0xFF로 XOR 연산을 수행한 뒤 암호화
-4. 암호화까지 완료되면 KAVM 문자열을 추가하고, 이 전체 내용의 MD5를 구한 다음, 암호화된 내용 뒤에 MD5를 추가
-5. 생성된 암호화 내용을 기존 확장자에서 kmd 확장자로 변경한 뒤 파일 생성
-'''
-
 #-*- coding:utf-8 -*-
 import sys
 import zlib
@@ -43,7 +34,7 @@ def main():
     buf4+=f #암호화된 내용 뒤에 MD5 추가
 
     kmd_name=fname.split('.')[0]+'.kmd'
-    fp=pen(kmd_name, 'wb')  #kmd 확장자로 암호파일 만들기
+    fp=open(kmd_name, 'wb')  #kmd 확장자로 암호파일 만들기
     fp.write(buf4)
     fp.close()
 
